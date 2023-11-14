51704c4ebb6df6f6d643f89bd9132f603e29d115
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-14 22:10:48 +0300

    add if_else

---
+++ Example005_ifElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ami")
+{
+    Console.WriteLine("И где ты была?");
+}
+else
+{
+    Console.Write("Привет: ");
+    Console.Write(username);
+}
