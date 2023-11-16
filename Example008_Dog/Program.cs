01396f61564d33026a8f77f378d322c7b93ab39c
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-16 16:07:30 +0300

    changed input data entry

--- Example008_Dog/Program.cs
+++ Example008_Dog/Program.cs
@@ -1,8 +1,13 @@
-﻿double distans = 1000;
-int speedFfirst = 2;
-int speedFsecond = 5;
-int speedDog = 12;
-int friend = 2;
+﻿Console.WriteLine("Введите Расстояние: ");
+double distans = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите скорость 1-ого друга: ");
+int speedFfirst = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость 2-ого друга: ");
+int speedFsecond = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость собаки: ");
+int speedDog = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите к какому другу бежит собака 1 или 2: ");
+int friend = Convert.ToInt32(Console.ReadLine());
 int count = 0;
 double time = 0;
 
