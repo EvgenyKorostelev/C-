f2667bc118af747678c0e57f05308fccabf96a8e
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-19 12:21:52 +0300

    add in lection003 sum 1 and 3 cifri 3-hznachnogo chisla

--- lecture003_/Program.cs
+++ lecture003_/Program.cs
@@ -1,8 +1,5 @@
-﻿int number = 9;
-int count = 2;
-while(count <= number)
-{
-    Console.Write(count);
-    Console.Write("\t");
-    count = count + 2;
-}
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+sum = (number / 100 + number % 10);
+Console.WriteLine($"Сумма первой и последней цифры трехзначного числа `{sum}`");
