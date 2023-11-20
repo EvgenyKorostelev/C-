82058a04f44c93f758d9c0ffba9da3171938d22d
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-20 21:10:05 +0300

    add lecture004

---
+++ Lecture004_/Program.cs
@@ -0,0 +1,12 @@
+﻿int num = 456;
+int a = num / 10;
+int b = a % 10;   // число, которое нужно возвести
+int c = num % 10; // степень
+int index = 1;
+int result = b;
+while (index < c)
+{
+    result = result * b;
+    index++;
+}
+Console.WriteLine(result);
