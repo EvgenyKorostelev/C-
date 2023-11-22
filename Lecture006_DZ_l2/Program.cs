b2913df06c488926692e38a04ad20c767f6c675f
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-22 10:52:48 +0300

    add Lecture006_DZ_l2 kratnoe chislo

---
+++ Lecture006_DZ_l2/Program.cs
@@ -0,0 +1,9 @@
+﻿int num = 161;
+if (num % 23 == 0 && num % 7 == 0)
+{
+    Console.WriteLine($"Число `{num}` кратно 7 и 23 одновременно");
+}
+else
+{
+    Console.WriteLine($"Число `{num}` НЕ кратно 7 и 23 одновременно");
+}
