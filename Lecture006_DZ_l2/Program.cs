a02b1c629e38801ec2bfafc6cf0d239550636905
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-22 13:18:10 +0300

    add Lecture008_DZ_l2

--- Lecture006_DZ_l2/Program.cs
+++ Lecture006_DZ_l2/Program.cs
@@ -1,4 +1,6 @@
-﻿int num = 161;
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
 if (num % 23 == 0 && num % 7 == 0)
 {
     Console.WriteLine($"Число `{num}` кратно 7 и 23 одновременно");
---
+++ Lecture008_DZ_l2/Program.cs
@@ -0,0 +1,22 @@
+﻿int a = 10, b = 99;
+Console.Write("Введите целое, двузначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if(num >= a && num <= b)
+{
+    int c = num / 10;
+    int d = num % 10;
+
+    if(c > d)
+    {
+        Console.WriteLine($"`{c}` наибольшая цифра числа `{num}`");
+    }
+    if(c < d)
+    {
+        Console.WriteLine($"`{d}` наибольшая цифра числа `{num}`");
+    }
+    if(c == d)
+    {
+        Console.WriteLine($"цифра`{c}` равна цифре `{d}` в числе `{num}`");
+    }
+}
