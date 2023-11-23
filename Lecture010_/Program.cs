b9974e297589bbe09909c44d0a7bcbb67c426269
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-23 21:30:29 +0300

    add Lecture011_

---
+++ Lecture010_/Program.cs
@@ -0,0 +1,27 @@
+﻿int TestFunc(int first){
+    if (first%2==0){
+        return first;
+    }
+    else{
+        return 0;
+    }
+}
+
+Console.WriteLine("enter first number");
+int first_number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter second number");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+if(first_number>second_number){
+    int temp = first_number;
+    first_number = second_number;
+    second_number = temp;
+}
+
+while(second_number>=first_number){
+    int res = TestFunc(first_number);
+    if(res !=0){
+        Console.Write($"{res} ");
+    }    
+    first_number++;
+}
---
+++ Lecture011_/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, 
+// которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//Console.Write("Введите целое число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//int Func(int sum)
+//{
+//    int sum = 0;
+//    int n = 1;
+//    while(n <= num)
+//    num = 1 + n;
+//    n++;
+//    return sum;
+//}
+//int vivod = Func(num);
+//Console.Write(vivod);
+
+//-----------------------------------------------------------------------------
+int NunFunc(int first){
+    int sum = 0;
+    for(int i=1;i<=first;i++){
+        sum+=i;
+    }
+    return sum;
+
+}
+
+Console.WriteLine("enter numb");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = NunFunc(num);
+Console.WriteLine(res);
+//------------------------------------------------------------------------------
+Console.WriteLine("enter numb");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = NunFunc(num);
+Console.WriteLine(res);
+
+void NunFunc(int first){
+    int sum = 0;
+    for(int i=1;i<=first;i++){
+        sum+=i;
+    }
+    Console.WriteLine(sum);
+
+}
+
+Console.WriteLine("enter numb");
+int num = Convert.ToInt32(Console.ReadLine());
+NunFunc(num);
