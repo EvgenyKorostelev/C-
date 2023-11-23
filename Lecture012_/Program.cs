986661cfaf273444fcfea3e51cf835873e13a6a7
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-23 21:45:47 +0300

    add Lecture012_

---
+++ Lecture012_/Program.cs
@@ -0,0 +1,23 @@
+﻿//Console.Write("Введите целое число: ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int count = 1;
+//while(num>10)
+//{
+//    num = num /10;
+//    count++;
+//}
+//Console.Write(count);
+
+//---------------------------------------------------------------------
+void NumCount(int num){
+    int count = 0;
+    while(num>0){
+        count++;
+        num /= 10;
+    }
+    Console.WriteLine($"Sum of digits is {count}");
+}
+
+Console.WriteLine("Enter your num");
+int num = Convert.ToInt32(Console.ReadLine());
+NumCount(num);
