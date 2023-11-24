76f6fade98f8a96959f5e916198dbbd450008202
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-24 10:46:35 +0300

    rework Lecture011_ and Lecture012_

--- Lecture012_/Program.cs
+++ Lecture012_/Program.cs
@@ -1,23 +1,31 @@
-﻿//Console.Write("Введите целое число: ");
-//int num = Convert.ToInt32(Console.ReadLine());
-//int count = 1;
-//while(num>10)
-//{
-//    num = num /10;
-//    count++;
-//}
-//Console.Write(count);
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
 
-//---------------------------------------------------------------------
-void NumCount(int num){
-    int count = 0;
-    while(num>0){
-        count++;
-        num /= 10;
-    }
-    Console.WriteLine($"Sum of digits is {count}");
+void Func(int numberTemp)
+{
+   int count = 0;
+   while(numberTemp>10)
+   {
+       numberTemp = numberTemp /10;
+       count++;
+   }
+   count++;
+   Console.Write(count);
 }
 
-Console.WriteLine("Enter your num");
-int num = Convert.ToInt32(Console.ReadLine());
-NumCount(num);
+Console.Write($"Количество цифр в числе {num}: ");
+Func(num);
+
+//---------------------------------------------------------------------
+//void NumCount(int num){
+//    int count = 0;
+//    while(num>0){
+//        count++;
+//        num /= 10;
+//    }
+//    Console.WriteLine($"Sum of digits is {count}");
+//}
+
+//Console.WriteLine("Enter your num");
+//int num = Convert.ToInt32(Console.ReadLine());
+//NumCount(num);
