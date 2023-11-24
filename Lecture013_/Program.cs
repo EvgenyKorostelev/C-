e95578a5d4045684585262b052d905d4442ada87
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-24 11:06:38 +0300

    add Lecture013_

---
+++ Lecture013_/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Func(int numTemp)
+{
+    int przv = 1;
+    int count = 1;
+    while(count <= numTemp)
+    {
+        przv = przv * count;
+        count++;
+    }
+    Console.Write(przv);
+}
+
+Console.Write($"Произведение чисел от 1 до {num}: ");
+Func(num);
