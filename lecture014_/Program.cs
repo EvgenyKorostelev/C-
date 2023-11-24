6f7a89e0987c77b8ca17fa67aa65593d5e2aa7ab
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-24 11:23:10 +0300

    add lecture014_

---
+++ lecture014_/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход число (N) и 
+//выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Func(int numTemp)
+{
+    int result = 0;
+    int count = 1;
+    while(count < numTemp)
+    {
+        result = count * count;
+        count++;
+        Console.Write(result + ", ");
+    }
+    result = count * count;
+    Console.Write(result + ".");
+}
+
+Console.Write($"Таблица квадратов чисел от 1 до {num}: ");
+Func(num);
