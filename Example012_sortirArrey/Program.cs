ba4e51d90466d1c4921f7559bf51e732ba6a4714
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-26 18:29:38 +0300

    add Example012_sortirArrey

---
+++ Example012_sortirArrey/Program.cs
@@ -0,0 +1,38 @@
+﻿//Сортировка массива через min/max
+
+int[] arr = {1, 2, 5, 7, 3, 9, 2, 5, 1};
+
+void PrintArrey(int[] arrey)
+{
+    int count = arrey.Length;
+
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{arrey[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void SelectionSort(int[] arrey)
+{
+    for(int i = 0; i < arrey.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for(int j = i + 1; j < arrey.Length; j++)
+        {
+            if(arrey[j] < arrey[minPosition])
+            {
+                minPosition = j;
+            }
+        }
+        int temp = arrey[i];
+        arrey[i] = arrey[minPosition];
+        arrey[minPosition] = temp;
+    }
+}
+
+PrintArrey(arr);
+SelectionSort(arr);
+PrintArrey(arr);
