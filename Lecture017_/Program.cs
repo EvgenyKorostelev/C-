8d613ed350fc0770f375c719653a89a194f3b1ca
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-29 10:14:40 +0300

    fix bug Lecture017_

--- Lecture017_/Program.cs
+++ Lecture017_/Program.cs
@@ -3,6 +3,25 @@
 //если элемент не имеет пары то записать его как последний в новом массиве
 //записать результаты в другой массив и вывести его
 
+int [] CreateArray(int min, int max, int size)
+{
+    int [] array = new int [size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max+1);
+    }
+    return array;
+}
+
+void PrintArray(int [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
 int[] CreateNewArray(int[] array)
 {
   int[] result = new int[array.Length / 2 + array.Length % 2];
@@ -25,7 +44,7 @@ int min = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter array max");
 int max = Convert.ToInt32(Console.ReadLine());
 
-int[] arr = CreateArray(max, min, size);
-ShowArray(arr);
+int[] arr = CreateArray(min, max, size);
+PrintArray(arr);
 int[] newArr = CreateNewArray(arr);
-ShowArray(newArr);
+PrintArray(newArr);
