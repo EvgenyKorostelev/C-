556e6e34da44b193cea7091cb13bf7e211c284ae
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-30 13:02:01 +0300

    add Example015_2xArray_Image s recursiei

--- Example014_2xArray/Program.cs
+++ Example014_2xArray/Program.cs
@@ -1,32 +1,30 @@
-﻿
+﻿// Заполнение двумерного массива
 void FillArray(int[,] mtrx)
 {
     for (int i = 0; i < mtrx.GetLength(0); i++)
     {
         for (int j = 0; j < mtrx.GetLength(1); j++)
         {
-            mtrx[i,j] = new Random().Next(1,10);  // [1,10)
+            mtrx[i, j] = new Random().Next(1, 10);  // [1, 10)
         }
     }
 }
 
-
-
-
+// Вывод двумерного массива на экран
 void PrintArray(int[,] mtrx)
 {
     for (int i = 0; i < mtrx.GetLength(0); i++)
     {
         for (int j = 0; j < mtrx.GetLength(1); j++)
         {
-            Console.Write($"{mtrx[i,j]} ");
+            Console.Write($"{mtrx[i, j]} ");
         }
         Console.WriteLine();
     }
 }
 
 
-int[,] matrix = new int [5,6];
+int[,] matrix = new int [5, 6];
 PrintArray(matrix);
 FillArray(matrix);
 Console.WriteLine();
---
+++ Example015_2xArray_Image/Program.cs
@@ -0,0 +1,22 @@
+﻿// Закрас области внутри контура на маленькой картинке
+void FillImage(int x, int y)
+{
+    if (pix[x, y] == 0)
+    {
+        pix[x, y] = 1;
+        FillImage(x - 1, y);
+        FillImage(x, y - 1);
+        FillImage(x + 1, y);
+        FillImage(x, y + 1);
+    }
+}
+
+// Рекурсия факториала
+int Factorial(int n)
+{
+    // 1! = 1
+    // 0! = 1
+    if(n == 1) return 1;
+    else 
+    return n * Factorial(n - 1);
+}
