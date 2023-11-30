608e53dc8a9dfa4fcc03681b86ba5f3065f68212
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-30 12:34:38 +0300

    add Example014_2xArray

---
+++ Example014_2xArray/Program.cs
@@ -0,0 +1,33 @@
+﻿
+void FillArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            mtrx[i,j] = new Random().Next(1,10);  // [1,10)
+        }
+    }
+}
+
+
+
+
+void PrintArray(int[,] mtrx)
+{
+    for (int i = 0; i < mtrx.GetLength(0); i++)
+    {
+        for (int j = 0; j < mtrx.GetLength(1); j++)
+        {
+            Console.Write($"{mtrx[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matrix = new int [5,6];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
