4d949fd57055d4612aebd7ecb7128ee347ebf040
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-11-30 13:37:57 +0300

    fix bug Example016_Fibanachy

---
+++ Example016_Fibanachy/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывод чисел фибаначи
+// f(1) = 1
+// f(2) = 1
+// f(n) = f(n - 1) + f(n - 2)
+
+int Fibanachy(int n)
+{
+    if(n == 1 || n == 2) return 1;
+    else 
+    return Fibanachy(n - 1) + Fibanachy(n - 2);
+}
+
+for (int i = 1; i < 20; i++)
+{
+    Console.WriteLine($"f({i}) = {Fibanachy(i)}");
+}
