e4e18ff2134a44acb404eec91da82959d3eaa78f
Evgeny Korostelev <ddc_smoke@mail.ru>
2023-12-07 21:39:55 +0300

    add  Lecture025_

---
+++ Lecture025_/Program.cs
@@ -0,0 +1,12 @@
+﻿// сумма натуральных чисел от 1 до n
+int Sum(int n)
+{
+    if(n == 1) return 1;
+    else return n + Sum(n - 1);
+}
+
+
+Console.WriteLine("Введите число n: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int result = Sum(x);
+Console.WriteLine(result);
