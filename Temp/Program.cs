8bfcf8f5559103dd6b1bfcda502eb3c2f830b781
Evgeny Korostelev <ddc_smoke@mail.ru>
2024-01-03 18:36:43 +0300

    edit Temp

--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,1 +1,4 @@
-﻿
+﻿//Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write($"Значение s: ");
+string s = Console.ReadLine() + "";
+Console.WriteLine($"Результат: {s}");
